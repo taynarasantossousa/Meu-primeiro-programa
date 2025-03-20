345dc105d5c18d3084a29d8f454e9e2431059fd9
Taynara <taynarasantossousa09@gmail.com>
2025-03-20 09:47:37 -0300

    Logica - da - programaçao -JS

---
+++ Script.js
@@ -0,0 +1,43 @@
+// var dado = "Taynara" /* String */
+// alert(typeof dado)
+
+// var dado = 123 /* Number */
+// alert(typeof dado)
+
+// vardado = true /* Boolean - true e false */
+// alert(typeof dado)
+
+// var dado  /* Underfined */
+// alert(typeof dado)
+
+
+
+/* OPERADORES ARITMÉTICOS */
+
+// ADIÇÃO            9 + 3 = 12
+// SUBTRAÇÃO         9 -3 = 6
+// DIVISÃO           9 / 3 = 3
+// MULTIPLICAÇÃO     9 * 3 = 27
+// POTÊNCIA          9 ** 3 = 729      9 * 9 * 9 
+// RESTO DA DIVISÃO  12 % 3 = 0    0,1 E 2
+
+
+var numero1=10
+var numero2=3
+
+var soma = numero1 + numero2
+
+alert(soma)
+
+
+
+  
+
+
+
+
+
+
+
+
+
