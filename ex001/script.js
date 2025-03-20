91e38cbe16840f766a23bc6063d096f51ecbda39
Taynara <taynarasantossousa09@gmail.com>
2025-03-20 13:55:06 -0300

    Exercicios-JS

---
+++ ex001/script.js
@@ -0,0 +1,5 @@
+var nome = prompt("Digite o seu nome")
+var sobrenome = prompt("Digite seu sobrenome")
+var nomeCompleto = nome + "" + sobrenome
+
+alert("Meu primeiro nome é " + nome + " e sobrenome é " + sobrenome + ". Nome completo é " + nomeCompleto)
